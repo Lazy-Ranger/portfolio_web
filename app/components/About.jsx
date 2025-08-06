4083959fa4044b608b59546452061e5902ef739e
Nitish <nitishkumar1@districtd.co>
2025-08-06 18:01:53 +0530

    feat(01) fist insiteate

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,15 +2,31 @@ import React from "react";
 import Image from "next/image";
 import { assets } from "../../assets/assets";
 
-
 const About = () => {
   return (
-    <div id="about" className="w-full px-[12%] py-10 scroll-mt-24">
-      <h4 className="text-center mb-2 text-lg font-Ovo" >About Me</h4>
-      <h2 className="text-center text-5xl font-Ovo">Introduction</h2>
-      <div>
-        <div>
-            <Image src={assets.user_image} alt="" className="w-20 rounded-full" />
+    <div id="about" className="w-full px-[12%] py-10 scroll-mt-24 bo">
+      <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
+      <h2 className="text-center text-5xl font-Ovo">About Me</h2>
+      <div className="flex w-full flex-col lg:flex-row item-center gap-20 my-20">
+        <div className="w-64 sm:w-80 rounded-3xl max-w-none">
+          <Image
+            src={assets.user_image}
+            alt="user"
+            className="w-full rounded-3xl"
+          />
+        </div>
+        <div className="flex-1">
+          <p>
+            I am experienced in the field of web development, and I have a
+            passion for creating innovative and user-friendly websites. I am
+            proficient in HTML, CSS, and JavaScript, and I have experience in
+            building responsive and mobile-friendly web applications.
+          </p>
+          <p>
+            I have a strong understanding of web design principles and
+            technologies, and I am skilled in using HTML, CSS, and JavaScript to
+            create visually appealing and interactive web pages.
+          </p>
         </div>
       </div>
     </div>
