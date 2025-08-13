ca1b6e8950d3c8d9d6f87cca09392da25c53f027
Nitish <nitishk642@gmail.com>
2025-08-13 23:47:38 +0530

    faat(01) working module

---
+++ app/components/Work.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+import { workData, assets } from "../../assets/assets";
+
+const Work = () => {
+  return (
+    <div id="work" className="w-full px-[12%] py-10 scroll-mt-24">
+      <h4 className="text-center mb-2 text-lg font-Ovo">My Portfolio</h4>
+      <h2 className="text-center text-5xl font-Ovo">My Latest Work</h2>
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-5 font-Ovo">
+        Welome to my portfolio, here you can see my latest work, I am a full
+        stack developer with a passion for creating innovative and user-friendly
+        websites. I am proficient in HTML, CSS, and JavaScript, and I have
+        experience in building responsive and mobile-friendly web applications.
+      </p>
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-5 my-10">
+        {workData.map((project, index) => {
+          return (
+            <div
+              className="aspects-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group h-50"
+              key={index}
+              style={{ backgroundImage: `url(${project.bgImage})` }}
+            >
+              <div
+                className="bg-white w-10/12 rounded-md absolute bottom-5 
+              left-1/2 -translate-x-1/2 py-3 px-5 flex items-center
+               justify-between duration-500 group-hover:bottom-7"
+              >
+                <div>
+                  <h2>{project.title}</h2>
+                  <p>{project.description}</p>
+                </div>
+                <div>
+                  <Image src={assets.send_icon} alt="" className="w-5" />
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Work;
